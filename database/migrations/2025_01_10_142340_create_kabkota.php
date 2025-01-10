33cf6ce792c22fa93c1c808b636be742c89852db
fajar <fajar23.septianto@gmail.com>
2025-01-10 21:55:00 +0700

    add database migration

---
+++ database/migrations/2025_01_10_142340_create_kabkota.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('kabkota', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->string('alt_nama')->nullable();
+            $table->decimal('lat');
+            $table->decimal('long');
+            $table->unsignedBigInteger('provinsi_id');
+            $table->foreign('provinsi_id')->references('id')->on('provinsi')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('kabkota');
+    }
+};
